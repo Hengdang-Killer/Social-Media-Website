a7f2d8548fbbad1a3ca32dd09b108b3d83a46bf0
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2023-07-31 23:14:51 +0530

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,23 @@
-import mongoose from "mongoose";
+/*-----------------------------------------------FUNCTION OF THE FILE--------------------------------------------*/
+/**
+ * This file is a module that defines a Mongoose schema for a "post" entity in the MongoDB database.
+ * This file provides a structure for storing and interacting with "post" data in the MongoDB database using Mongoose, and it allows us to perform CRUD operations (Create, Read, Update, Delete) on the "posts" collection. Note that the likes field uses a Map to store likes for each post, and the comments field is an array that holds comments for each post, initialized with an empty array as the default value.
+ */
 
+
+
+
+/*------------------------------------------------IMPORTING LIBRARIES------------------------------------------*/
+import mongoose from "mongoose";
+/**
+ *  This line imports the Mongoose library.
+ */
+
+
+
+
+
+/*------------------------------------------------SCHEMA FOR A POST------------------------------------------*/
 const postSchema = mongoose.Schema(
   {
     userId: {
@@ -29,7 +47,27 @@ const postSchema = mongoose.Schema(
   },
   { tinestamps: true }
 );
+/**
+ * This defines the schema for the "post" entity using Mongoose. It has several fields, such as userId, firstName, lastName, location, description, picturePath, userPicturePath, likes, and comments. 
+ * The timestamps: true option automatically adds createdAt and updatedAt fields to the documents, which track when the documents were created and last updated, respectively.
+ */
 
+
+
+
+/*------------------------------------------------CREATING THE MODEL OF THE ABOVE SCHEMA------------------------------------------*/
 const Post = mongoose.model("Post", postSchema);
+/**
+ * This creates a Mongoose model named "Post" using the defined schema. 
+ * The model is associated with the "posts" collection in the MongoDB database.
+ */
 
+
+
+
+
+/*------------------------------------------------EXPORTING THE MODEL------------------------------------------*/
 export default Post;
+/**
+ * This exports the "Post" model as the default export of the module, allowing other files to import and use it.
+ */
